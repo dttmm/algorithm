d5645e137999f33a2f4974f0798baba9463b76f5
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-01-28 14:39:42 +0900

    수업 연습문제

---
+++ src/Pratice.java
@@ -0,0 +1,19 @@
+public class Pratice {
+    public static void main(String[] args) {
+        int[] arr = {-1,3,-9,6,7,-6,1,5,4,-2};
+        int count =0;
+        for (int i = 1; i < (1 << arr.length) ; i++) {
+            int sum = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if ((i & (1 << j)) == 1) {
+                    sum += arr[j];
+                    System.out.print(arr[j]+" ");
+                }
+            }
+            if (sum ==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}

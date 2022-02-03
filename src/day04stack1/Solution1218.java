e8acebd6f26e07f60da630c75d4113e7ee4d7849
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-02-03 17:59:13 +0900

    2월3일(목) 스택1

---
+++ src/day04stack1/Solution1218.java
@@ -0,0 +1,43 @@
+package day04stack1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Solution1218 {
+
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/input_day04_1218.txt"));
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = 10;
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = sc.nextInt();
+            String s = sc.next();
+            char[] cArr = new char[N];
+
+            int cur = 0;
+            int flag = 1;
+
+            for (int i = 0; i < N; i++) {
+                char c = s.charAt(i);
+                if (c == '{' || c == '[' || c == '<' || c == '(') {
+                    cArr[cur++] = c;
+                } else if (c == '}' && cArr[cur - 1] == '{') {
+                    cur--;
+                } else if (c == ']' && cArr[cur - 1] == '[') {
+                    cur--;
+                } else if (c == '>' && cArr[cur - 1] == '<') {
+                    cur--;
+                } else if (c == ')' && cArr[cur - 1] == '(') {
+                    cur--;
+                } else {
+                    flag = 0;
+                    break;
+                }
+            }
+            System.out.println("#" + test_case + " " + flag);
+        }
+    }
+}

2d4bfd60b574a22046ae49a96e4cdea909e59560
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-02-04 18:26:36 +0900

    2월4일(금) 스택2

---
+++ src/day05stack2/Solution1224.java
@@ -0,0 +1,74 @@
+package day05stack2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution1224 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/input_day05_1224.txt"));
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = 10;
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            int N = sc.nextInt();
+            char[] arr = sc.next().toCharArray();
+            Stack<Character> st = new Stack<>();
+            Stack<Integer> st_cal = new Stack<>();
+            char[] formula = new char[N];
+            int top = -1;
+            int start = -1;
+
+            for (int i = 0; i < N; i++) {
+                char c = arr[i];
+                if (c == '(') {
+                    st.push(c);
+                } else if (c == '*' || c == '/') {
+                    while (!st.isEmpty() && (st.peek() == '*' || st.peek() == '/')) {
+                        formula[++top] = st.pop();
+                    }
+                    st.push(c);
+                } else if (c == '+' || c == '-') {
+                    while (!st.isEmpty() && (st.peek() == '*' || st.peek() == '/' || st.peek() == '+' || st.peek() == '-')) {
+                        formula[++top] = st.pop();
+                    }
+                    st.push(c);
+                } else if (c == ')') {
+                    while (st.peek() != '(') {
+                        formula[++top] = st.pop();
+                    }
+                    st.pop();
+                } else {
+                    formula[++top] = c;
+                }
+            }
+            while (!st.isEmpty()) {
+                formula[++top] = st.pop();
+            }
+
+            while (start != top) {
+                char c = formula[++start];
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
+                    int a = st_cal.pop();
+                    int b = st_cal.pop();
+                    if (c == '+') {
+                        st_cal.push(b + a);
+                    } else if (c == '-') {
+                        st_cal.push(b - a);
+                    } else if (c == '*') {
+                        st_cal.push(b * a);
+                    } else {
+                        st_cal.push(b / a);
+                    }
+                } else {
+                    st_cal.push(c - '0');
+                }
+            }
+
+            System.out.println("#" + test_case + " " + st_cal.pop());
+        }
+    }
+}
---
+++ src/day05stack2/Solution1267.java
@@ -0,0 +1,52 @@
+package day05stack2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution1267 {
+    static int[][] arr;
+    static boolean[] visited;
+    static int V;
+
+
+    public static void DFS(int v) {
+        if (!visited[v]) {
+            for (int i = 0; i < V; i++) {
+                if (arr[v][i] == 1 && !visited[i]) {
+                    DFS(i);
+                }
+            }
+        }
+        visited[v] = true;
+        System.out.print(1 + v + " ");
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/input_day05_1267.txt"));
+
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T = 10;
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            V = sc.nextInt();
+            int E = sc.nextInt();
+            arr = new int[V][V];
+            visited = new boolean[V];
+            for (int i = 0; i < E; i++) {
+                int from = sc.nextInt();
+                int to = sc.nextInt();
+                arr[to - 1][from - 1] = 1;
+            }
+            System.out.print("#" + test_case + " ");
+            for (int i = 0; i < V; i++) {
+                if (!visited[i]) {
+                    DFS(i);
+                }
+            }
+            System.out.println();
+        }
+    }
+}

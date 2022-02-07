96b6892d2004d4ba66b75890698d62d55829e9e0
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-02-07 17:04:44 +0900

    2월7일(월) 큐

---
+++ src/day06queue/Practice.java
@@ -0,0 +1,42 @@
+package day06queue;
+
+public class Practice {
+    static int[][] G;
+    static int[] q = new int[10];
+    static int front = -1;
+    static int rear = -1;
+    static boolean[] visited = new boolean[8];
+
+    public static void BFS() {
+
+        while (!(front == rear)) {
+            int v = q[++front];
+            for (int i = 1; i <= G[v][0]; i++) {
+                int w = G[v][i];
+                if (!visited[w]) {
+                    visited[w] = true;
+                    q[++rear] = w;
+                    System.out.print(w + " ");
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        G = new int[8][8];
+
+        char[] c = "1213242546566737".toCharArray();
+        for (int i = 0; i < 8; i++) {
+            int from = c[2 * i] - '0';
+            int to = c[2 * i + 1] - '0';
+            G[from][++G[from][0]] = to;
+        }
+
+
+        System.out.print(1 + " ");
+        q[++rear] = 1;
+        visited[1] = true;
+        BFS();
+    }
+}
---
+++ src/day06queue/Practice2.java
@@ -0,0 +1,68 @@
+package day06queue;
+
+import java.util.Scanner;
+
+public class Practice2 {
+    static int[] qu;
+    static int front;
+    static int rear;
+    static int total;
+    static int[] visited;
+    static Scanner scanner;
+    static int count;
+    static int size;
+
+    public static void myzzu() {
+
+        System.out.println("현재 큐에 있는 사람");
+        for (int i = 0; i < size; i++) {
+            System.out.print(qu[(front + 1 + i) % 20] + " ");
+        }
+        System.out.println();
+        System.out.println("현재 일인당 나눠주는 사탕의 수");
+        for (int i = 0; i < 20; i++) {
+            if (visited[i] != 0) {
+                System.out.printf("%d: %d개", i + 1, visited[i]);
+            }
+        }
+        System.out.println();
+        System.out.println("현재 일인당 나눠준 사탕의 수");
+        System.out.println(20 - total);
+
+        front = (front + 1) % 20;
+        int cur = qu[front];
+
+        visited[cur - 1]++;
+        size--;
+        if (total - visited[cur - 1] <= 0) {
+            System.out.printf("20번째주인공 %d\n", cur);
+        } else {
+            total -= visited[cur - 1];
+
+            rear = (rear + 1) % 20;
+            qu[rear] = cur;
+            size++;
+            rear = (rear + 1) % 20;
+            qu[rear] = ++count;
+            size++;
+            myzzu();
+        }
+    }
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        qu = new int[20];
+        front = -1;
+        rear = -1;
+        total = 20;
+        visited = new int[20];
+        count = 1;
+        size = 0;
+
+        qu[++rear] = 1;
+        size++;
+        myzzu();
+
+
+    }
+}

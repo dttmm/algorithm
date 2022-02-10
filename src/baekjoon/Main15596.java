f79bd2d390bd0088374e170bb9ee41bf31077b51
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-02-11 00:37:27 +0900

    백준 | 15596 정수 N개의 합 | 브론즈2 | 3분 20초

---
+++ src/baekjoon/Main15596.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class Main15596 {
+    long sum(int[] a) {
+        long sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        return sum;
+    }
+}

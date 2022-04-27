9769ca9155a8556c43b589c2bbbc9f63ad52029e
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-04-27 22:12:48 +0900

    백준 | 9372 상근이의 여행 | 실버3 | 26분
    답 어이없네

---
+++ src/baekjoon/Main9372.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main9372 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/baekjoon/9372.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            for (int i = 0; i < M; i++) {
+                br.readLine();
+            }
+            System.out.println(N - 1);
+        }
+    }
+}

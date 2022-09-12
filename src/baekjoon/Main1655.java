f1b153c3f9456ff67cf75296d0615a0378ee9fbf
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-09-12 17:54:28 +0900

    백준 | 1655 가운데를 말해요 | 골드2 | 1시간 50분
    아 BufferedWriter 안써서 시간초과 되었던 거였네

--- src/baekjoon/Main1655.java
+++ src/baekjoon/Main1655.java
@@ -1,98 +1,163 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main1655 {
 
-    static Node head;
-    static Node tail;
-    static Node mid;
+    final static int MAX_SIZE = 100000;
     static int size;
+    static int mid;
+    static LowPQ lowPQ;
+    static HighPQ highPQ;
+    static BufferedWriter bw;
 
-    private static class Node {
-        int data;
-        Node next;
-        Node pre;
+    private static class LowPQ {
+        int heap[] = new int[MAX_SIZE * 2];
+        int heapSize = 0;
 
-        public Node(int data) {
-            this.data = data;
+        void heapPush(int value) {
+            if (heapSize + 1 > MAX_SIZE) {
+                return;
+            }
+
+            heap[heapSize] = value;
+
+            int current = heapSize;
+            while (current > 0 && heap[current] < heap[(current - 1) / 2]) {
+                int temp = heap[(current - 1) / 2];
+                heap[(current - 1) / 2] = heap[current];
+                heap[current] = temp;
+                current = (current - 1) / 2;
+            }
+
+            heapSize++;
+        }
+
+        int heapPop() {
+            if (heapSize <= 0) {
+                return -1;
+            }
+
+            int value = heap[0];
+            heapSize--;
+
+            heap[0] = heap[heapSize];
+
+            int current = 0;
+            while (current < heapSize && current * 2 + 1 < heapSize) {
+                int child;
+                if (current * 2 + 2 >= heapSize) {
+                    child = current * 2 + 1;
+                } else {
+                    child = heap[current * 2 + 1] < heap[current * 2 + 2] ? current * 2 + 1 : current * 2 + 2;
+                }
+
+                if (heap[current] < heap[child]) {
+                    break;
+                }
+
+                int temp = heap[current];
+                heap[current] = heap[child];
+                heap[child] = temp;
+
+                current = child;
+            }
+            return value;
+        }
+    }
+
+    private static class HighPQ {
+        int heap[] = new int[MAX_SIZE * 2];
+        int heapSize = 0;
+
+        void heapPush(int value) {
+            if (heapSize + 1 > MAX_SIZE) {
+                return;
+            }
+
+            heap[heapSize] = value;
+
+            int current = heapSize;
+            while (current > 0 && heap[current] > heap[(current - 1) / 2]) {
+                int temp = heap[(current - 1) / 2];
+                heap[(current - 1) / 2] = heap[current];
+                heap[current] = temp;
+                current = (current - 1) / 2;
+            }
+
+            heapSize++;
+        }
+
+        int heapPop() {
+            if (heapSize <= 0) {
+                return -1;
+            }
+
+            int value = heap[0];
+            heapSize--;
+
+            heap[0] = heap[heapSize];
+
+            int current = 0;
+            while (current < heapSize && current * 2 + 1 < heapSize) {
+                int child;
+                if (current * 2 + 2 >= heapSize) {
+                    child = current * 2 + 1;
+                } else {
+                    child = heap[current * 2 + 1] > heap[current * 2 + 2] ? current * 2 + 1 : current * 2 + 2;
+                }
+
+                if (heap[current] > heap[child]) {
+                    break;
+                }
+
+                int temp = heap[current];
+                heap[current] = heap[child];
+                heap[child] = temp;
+
+                current = child;
+            }
+            return value;
         }
     }
 
     public static void insert(int data) {
-        Node newNode = new Node(data);
 
         // 데이터 집어 넣기
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-            mid = newNode;
+        if (size == 1) {
+            mid = data;
         } else {
-            // head보다 작을 때
-            if (newNode.data <= head.data) {
-                head.pre = newNode;
-                newNode.next = head;
-                head = newNode;
+            // 미드보다 작은 경우
+            if (data < mid) {
+                highPQ.heapPush(data);
             }
-            // tail보다 클 때
-            else if (newNode.data >= tail.data) {
-                tail.next = newNode;
-                newNode.pre = tail;
-                tail = newNode;
-            } else {
+            // 미드보다 큰 경우
+            else {
+                lowPQ.heapPush(data);
+            }
+
+            // mid값 조정하기
+            // 짝수일 때
+            if (size % 2 == 0) {
                 // mid보다 작을 때
-                if (newNode.data < mid.data) {
-                    Node node = head;
-                    while (true) {
-                        if (newNode.data < node.next.data) {
-                            newNode.next = node.next;
-                            newNode.pre = node;
-                            node.next.pre = newNode;
-                            node.next = newNode;
-                            break;
-                        }
-                        node = node.next;
-                    }
+                if (data < mid) {
+                    lowPQ.heapPush(mid);
+                    mid = highPQ.heapPop();
                 }
-                // mid 보다 클 때
+                // mid보다 클 때는 변함없음
                 else {
-                    Node node = mid;
-                    while (true) {
-                        if (newNode.data < node.next.data) {
-                            newNode.next = node.next;
-                            newNode.pre = node;
-                            node.next.pre = newNode;
-                            node.next = newNode;
-                            break;
-                        }
-                        node = node.next;
-                    }
                 }
             }
-        }
-
-        // mid값 조정하기
-
-        // 짝수일 때
-        if (size % 2 == 0) {
-            // mid보다 작을 때
-            if (newNode.data < mid.data) {
-                mid = mid.pre;
-            }
-            // mid보다 클 때는 변함없음
+            // 홀수일 때
             else {
-            }
-        }
-        // 홀수일 때
-        else {
-            // mid보다 작을 때는 변함없음
-            if (newNode.data <= mid.data) {
-            }
-            // mid보다 클 때
-            else {
-                mid = mid.next;
+                // mid보다 작을 때는 변함없음
+                if (data < mid) {
+                }
+                // mid보다 클 때
+                else {
+                    highPQ.heapPush(mid);
+                    mid = lowPQ.heapPop();
+                }
             }
         }
     }
@@ -101,16 +166,22 @@ public class Main1655 {
         System.setIn(new FileInputStream("res/baekjoon/1655.txt"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         size = 0;
+        lowPQ = new LowPQ();
+        highPQ = new HighPQ();
 
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
 
             size++;
             insert(num);
-            System.out.println(mid.data);
+            bw.write("" + mid);
+            bw.newLine();
         }
+
+        bw.close();
     }
 }

1d8f198e4eb7cceba7e473db89f17f8c3e01363b
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-10-31 21:05:15 +0900

    프로그래머스 | 옹알이 (1) | Lv. 0 | 10분 레벨 0이 순열이 나온다고?

---
+++ src/programmers/lv0/옹알이1.java
@@ -0,0 +1,48 @@
+package programmers.lv0;
+
+import java.util.*;
+
+class Solution {
+
+    String[] arr = {"aya", "ye", "woo", "ma"};
+    Map<String, Boolean> map;
+    String[] tr;
+    boolean[] visited;
+
+    void solve(int k, int R) {
+        if (k == R) {
+            String s = "";
+            for (int i = 0; i < R; i++) {
+                s += tr[i];
+            }
+            map.put(s, true);
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    tr[k] = arr[i];
+                    solve(k + 1, R);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+
+    public int solution(String[] babbling) {
+        int answer = 0;
+        map = new HashMap();
+
+        // map init
+        for (int i = 1; i <= 4; i++) {
+            tr = new String[i];
+            visited = new boolean[4];
+            solve(0, i);
+        }
+
+        for (int i = 0; i < babbling.length; i++) {
+            if (map.containsKey(babbling[i])) answer++;
+        }
+
+        return answer;
+    }
+}

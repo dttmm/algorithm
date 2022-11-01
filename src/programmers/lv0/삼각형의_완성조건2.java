c63eff6e0b43f200ac105a9b166a5bbec55bbcf5
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-11-01 22:31:15 +0900

    프로그래머스 | 삼각형의 완성조건 (2) | Lv. 0 | 7분

---
+++ src/programmers/lv0/삼각형의_완성조건2.java
@@ -0,0 +1,17 @@
+package programmers.lv0;
+
+import java.util.*;
+
+class 삼각형의_완성조건2 {
+    public int solution(int[] sides) {
+        int answer = 0;
+
+        //가능한 범위: (두수의 합 -1) ~ (두수의 차 +1) +1
+        int sum = sides[0] + sides[1];
+        int diff = Math.abs(sides[0] - sides[1]);
+
+        answer = (sum - 1) - (diff + 1) + 1;
+
+        return answer;
+    }
+}

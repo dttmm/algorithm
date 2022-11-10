05aee03853cc88b6303d6a2714fb7a90dfce9066
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-11-10 21:38:39 +0900

    파일 정리

--- src/programmers/lv0/No_008_로그인_성공.java
+++ src/programmers/lv0/No_008_로그인_성공.java
@@ -2,28 +2,31 @@ package programmers.lv0;
 
 import java.util.*;
 
-class Solution {
+class No_008_로그인_성공 {
 
-    Map<String, String> map;
+    class Solution {
 
-    public String solution(String[] id_pw, String[][] db) {
-        String answer = "";
-        map = new HashMap();
+        Map<String, String> map;
 
-        for (int i = 0; i < db.length; i++) {
-            String id = db[i][0];
-            String pw = db[i][1];
+        public String solution(String[] id_pw, String[][] db) {
+            String answer = "";
+            map = new HashMap();
 
-            map.put(id, pw);
+            for (int i = 0; i < db.length; i++) {
+                String id = db[i][0];
+                String pw = db[i][1];
+
+                map.put(id, pw);
+            }
+
+            String id = id_pw[0];
+            String pw = id_pw[1];
+            if (map.containsKey(id)) {
+                if (map.get(id).equals(pw)) answer = "login";
+                else answer = "wrong pw";
+            } else answer = "fail";
+
+            return answer;
         }
-
-        String id = id_pw[0];
-        String pw = id_pw[1];
-        if (map.containsKey(id)) {
-            if (map.get(id).equals(pw)) answer = "login";
-            else answer = "wrong pw";
-        } else answer = "fail";
-
-        return answer;
     }
 }

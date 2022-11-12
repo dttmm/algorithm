db2863daa2e6e48a0415ef9f1bd71183ccf0eacf
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-11-12 19:16:40 +0900

    프로그래머스 | 분수의 덧셈 | Lv. 0 | 14분 너무 복잡하게 생각했다 그냥 분수 계산하고 약분해주면 되는거였네

--- src/programmers/lv0/No_019_분수의_덧셈.java
+++ src/programmers/lv0/No_019_분수의_덧셈.java
@@ -3,30 +3,16 @@ package programmers.lv0;
 public class No_019_분수의_덧셈 {
 
     class Solution {
+        public String solution(String my_string, int num1, int num2) {
+            String answer = "";
 
-        int gcd(int a, int b) {
-            if (b == 0) return a;
-            return gcd(b, a % b);
-        }
+            char[] arr = my_string.toCharArray();
 
-        public int[] solution(int denum1, int num1, int denum2, int num2) {
+            char temp = arr[num1];
+            arr[num1] = arr[num2];
+            arr[num2] = temp;
 
-            int n = gcd(num1, num2);
-            int n1 = num1 / n;
-            int n2 = num2 / n;
-
-            int max = n1 * n2 * n; // 최소공배수
-
-            denum1 *= max / num1;
-            denum2 *= max / num2;
-
-            int denum = denum1 + denum2;
-
-            n = gcd(denum, max); // 최종 분자 분모의 gcd
-            denum /= n;
-            max /= n;
-
-            int[] answer = {denum, max};
+            answer = String.valueOf(arr);
 
             return answer;
         }

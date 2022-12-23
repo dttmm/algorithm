c3b85925926585fa15b1b9808cc029f54d713e14
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-12-23 20:26:51 +0900

    백준 | UCPC에서 가장 쉬운 문제 번호는? | 브론즈5 | 3분

---
+++ src/baekjoon/Main25311.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Main25311 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/baekjoon/25311.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("A");
+    }
+}
